34ec6be55d914b2150101235b0829be895678d99
kergeres <55786735+kergeres@users.noreply.github.com>
2022-01-25 18:30:55 +0100

    alt map

---
+++ script/main.js
@@ -0,0 +1,119 @@
+"use strict";
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-storage.js"
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-analytics.js";
+
+// Firebase configuration
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCdEcafmWt0TPFWqbN714rwZp7v_IXFfJc",
+  authDomain: "style-rules-for-men.firebaseapp.com",
+  projectId: "style-rules-for-men",
+  storageBucket: "style-rules-for-men.appspot.com",
+  messagingSenderId: "593080027609",
+  appId: "1:593080027609:web:193ac26d9f603be9cdf1c8",
+  measurementId: "G-W5Y6W84K2X"
+};
+
+// google analytics initialize 
+// window.dataLayer = window.dataLayer || [];
+// function gtag() { dataLayer.push(arguments); }
+// gtag('js', new Date());
+
+// gtag('config', 'G-LV81HRN89M');
+
+
+
+// // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
+// const app = initializeApp(firebaseConfig);
+
+// const db = firebase.firestore();
+// const rulesRef = db.collection("rules");
+
+// let database = [];
+// rulesRef.onSnapshot(function (snapshotData) {
+
+//   snapshotData.forEach(doc => {
+//     let dt = doc.data();
+//     dt.id = doc.id;
+//     database.push(dt);
+
+//   }
+//   );
+//   // appendNav(database)
+// });
+
+// // search functions by searchbar in firebase database 
+// document.querySelector(".search-input").addEventListener('keyup', (e) => {
+//   let srchValue = document.querySelector('.search-input').value.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '')
+//   searchPrograms(srchValue)
+// })
+
+// function searchPrograms(value) {
+
+//   let filteredPrograms = []
+//   for (const item of database) {
+//     let title = item.monster.mname.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+
+//     if (title.includes(value.toLowerCase())) {
+//       filteredPrograms.push(item);
+//     }
+//   }
+
+//   appendNav(filteredPrograms);
+// }
+// // append the creatures name list on the list bar  
+// function appendNav(items) {
+//   let temlplete = ""
+
+//   for (let item of items) {
+//     temlplete += `<label for="mobileicon"><p class="navAppended">${item.title}</p></label> `
+
+//   }
+
+//   document.querySelector(".item-title-container").innerHTML = temlplete
+//   clickListener()
+
+// }
+
+let clickListener = () => {
+
+  let cbox = document.querySelectorAll(".navAppended");
+  cbox.forEach(box => {
+    box.addEventListener('click', (e) => {
+      appendProfile(e.target.id)
+
+    })
+  }
+  )
+}
+clickListener()
+
+let tieT = `
+<h3>izebigyo</h3>
+
+`
+
+// SPA selected data page to the dom 
+function appendProfile(chosenItemName) {
+
+  document.querySelector(".content-container").innerHTML = eval(chosenItemName)
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ script/nav.js
@@ -0,0 +1,37 @@
+"use strict"
+
+
+// hamburger menu opener 
+function hamburgerNav() {
+
+    let cbOpen = document.querySelector('#mobileicon');
+    if (cbOpen.checked == false)//ha zarva van
+    {
+        document.querySelector(".nnav").classList.remove("nav-open");
+        document.querySelector(".ham-menu-line").classList.remove("line-rot");
+        document.querySelector(".ham-menu-linea").classList.remove("linea-rot");
+        document.querySelector(".ham-menu-lineb").classList.remove("lineb-rot");
+        document.querySelector(".ham-cont").style.left = "20px";
+    }
+
+    else {
+        document.querySelector(".nnav").classList.add("nav-open");
+        document.querySelector(".ham-menu-line").classList.add("line-rot");
+        document.querySelector(".ham-menu-linea").classList.add("linea-rot");
+        document.querySelector(".ham-menu-lineb").classList.add("lineb-rot");
+        document.querySelector(".ham-cont").style.left = "240px";
+    }
+}
+document.querySelector('.ham-cont').addEventListener('click', hamburgerNav)
+
+// header shrinker 
+window.addEventListener('scroll', () => {
+    let headerBar = document.querySelector(".header-bar")
+    let headerLogo = document.querySelector(".header-logo-dktp")
+    let headerBarH1 = document.querySelector(".header-bar > h1")
+    let headerBarB = document.querySelector(".header-bar > h1 > b")
+    headerBar.classList.toggle('scroll-header-shrinker', window.scrollY > 0)
+    // headerLogo.classList.toggle('scroll-logo-shrinker', window.scrollY > 0)
+    headerBarH1.classList.toggle('scroll-h1-shrinker', window.scrollY > 0)
+    headerBarB.classList.toggle('scroll-b-shrinker', window.scrollY > 0)
+})
