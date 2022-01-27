42e91c9a42517d0425989ed1c4478592a2ca3380
kergeres <55786735+kergeres@users.noreply.github.com>
2022-01-28 00:19:18 +0100

    ads script added.

--- script/main.js
+++ script/main.js
@@ -98,7 +98,85 @@ let clickListener = () => {
 clickListener()
 
 let tieT = `
-<h3>izebigyo</h3>
+<h1>the Tie</h1>
+<h2>Tie types</h2>
+<p>
+    We can separate the ties into two main groups, the bowl and plain ties. The Occasion and clothing will
+    decide which one can we choose. The bowl tie exclusively match with tuxedo (every other clothe should be
+    avoided). Wear a bow tie exclusively for tuxedos.
+    For the blazers and suit jackets you should wear a normal, hand knotted tie.
+    <br><br>
+    For a men it is important to tie his own tie, newer wear a pre tied tie.
+
+    <!-- két fő csoportra oszthatjuk a nyakkendőket, a csokor és a sima nyekkendőkre. alkalomtól és az öltözékünk
+    többi
+    részétől függ hogy melyiket kell válasszuk. Csokornyakkendőt kizárolag smokinghoz vegyünk fel.
+    Meg a sima nyakkendot zakokhoz blazerhez es vehetjuk fel. -->
+</p>
+<h2>How to tie a tie</h2>
+<p>
+    There are many ways to tie a tie, but like most, in this case a
+    simplest solution is best. <i>Oriental Knot</i> is not only easy to tie but perfect for all occasions
+    and like the real style marks it doesn't go out of style either.
+
+    <!-- Sok féle módon lehet megkötni egy nyakkendőt, viszont mint legtöbbször, ebben az esetben is a
+    legegyszerubb megoldas a legjobb. Az Oriental Knot nem csak konnyen megkotheto de minden alkalomra
+    tokeletes es mint az igazi stilusjegyek ez sem megy ki a divatbol. -->
+
+</p>
+
+
+<div class="steps-container">
+
+    <div class="step-card">
+        <img class="steps-img" src="img/tie-knot 1.jpg">
+        <p>Start with the backside of the tie facing away from you, the wide end is on the right and the
+            small end is on the left. The tip of the small end should rest slightly above your belly-button
+            (this will vary depending on your height and the length & thickness of your tie). Move only the
+            active (wide) end.</p>
+    </div>
+    <div class="step-card">
+        <img class="steps-img" src="img/tie-knot 2.jpg">
+        <p>Wide end under the small end to the left.</p>
+    </div>
+    <div class="step-card">
+        <img class="steps-img" src="img/tie-knot 3.jpg">
+        <p>Across the small end to the right.</p>
+    </div>
+    <div class="step-card">
+        <img class="steps-img" src="img/tie-knot 4.jpg">
+        <p>Up into the neck loop from underneath.</p>
+    </div>
+    <div class="step-card">
+        <img class="steps-img" src="img/tie-knot 5.jpg">
+        <p>Down through the loop you've just made in the front.</p>
+    </div>
+    <div class="step-card">
+        <img class="steps-img" src="img/tie-knot 6.jpg">
+        <p>Pull down on the wide end to tighten. Slide the knot up to adjust.</p>
+    </div>
+
+</div>
+
+
+<h2>Tie knot types</h2>
+<img class="img-med" src="img/Tie-Knots.jpg">
+<h2>Tie Rules</h2>
+<ul>
+    <li>The tie's width has to be the same as the shirt's collar.</li>
+    <!-- <li>A nyakkendő ugyanolyan széles legyen, mint a gallér.</li> -->
+    <li>The tie's width has to be just as long it touches the belt line.</li>
+    <!-- <li>A nyakkendő pont az öv vonaláig érjen.</li> -->
+    <li>The tie's color has to be darker than the shirt.</li>
+    <!-- <li>A nyakendő mindig legyen sötétebb az ingnél </li> -->
+    <li>The tie's pattern or colour can't be the same as the pocket square.</li>
+    <!-- <li>a nyakkendő mintaja sohasem egyezik az ing vagy a zsebkendő mintájával- </li> -->
+    <li>The tie's classic colours are black, navy blue and red.</li>
+    <li>If you want to wear a patterned tie, choose one with a small, light coloured patterns.</li>
+    <li>If you use a tiebar it should go between the third and fourth buttons of your dress shirt. </li>
+    <li>Never wear a pre tied tie.</li>
+    <li>Never wear a shiny tie.</li>
+</ul>
 
 `
 
