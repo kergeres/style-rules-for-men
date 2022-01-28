781e8f777cbaa28d4c9c1faf718892059448ec5f
kergeres <55786735+kergeres@users.noreply.github.com>
2022-01-29 00:14:40 +0100

    outfolder step. pocket square added

---
+++ web/script/nav.js
@@ -0,0 +1,48 @@
+"use strict"
+
+
+// hamburger menu opener 
+function hamburgerNav() {
+    console.log("asdf");
+    let cbOpen = document.querySelector('#mobileicon');
+    if (cbOpen.checked == false)//ha zarva van
+    {
+        document.querySelector(".nnav").classList.remove("nav-open");
+        document.querySelector(".ham-menu-line").classList.remove("line-rot");
+        document.querySelector(".ham-menu-linea").classList.remove("linea-rot");
+        document.querySelector(".ham-menu-lineb").classList.remove("lineb-rot");
+        document.querySelector(".ham-cont").style.left = "20px";
+
+    }
+
+    else {
+        document.querySelector(".nnav").classList.add("nav-open");
+        document.querySelector(".ham-menu-line").classList.add("line-rot");
+        document.querySelector(".ham-menu-linea").classList.add("linea-rot");
+        document.querySelector(".ham-menu-lineb").classList.add("lineb-rot");
+        document.querySelector(".ham-cont").style.left = "240px";
+    }
+}
+document.querySelector('.ham-cont').addEventListener('click', hamburgerNav)
+
+
+// header shrinker 
+window.addEventListener('scroll', () => {
+    let headerBar = document.querySelector(".header-bar")
+    let headerLogo = document.querySelector(".header-logo-dktp")
+    let headerBarH1 = document.querySelector(".header-bar > h1")
+    let nav = document.querySelector("nav")
+
+    let completor = document.querySelector(".headercompletor")
+    headerBar.classList.toggle('scroll-header-shrinker', window.scrollY > 0)
+    // headerLogo.classList.toggle('scroll-logo-shrinker', window.scrollY > 0)
+    headerBarH1.classList.toggle('scroll-h1-shrinker', window.scrollY > 0)
+
+    nav.classList.toggle('nav-padd-top', window.scrollY > 0)
+
+
+
+
+    completor.classList.toggle('headercompletor-shrinker', window.scrollY > 0)
+
+})
